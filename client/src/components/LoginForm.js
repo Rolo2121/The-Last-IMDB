a80a19906627a0e8a8fc8e2d4a9759937c68cb98
Jacob  Bauer <jacobbauer@Jacobs-MacBook-Air.local>
2022-08-18 12:36:55 -0500

    new files

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,18 @@
+// import React, { useState } from 'react';
+// import { loginUser } from '../utils/API';
+// import Auth from '../utils;
+
+// const LoginForm = () => {
+//     const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+//     const [validated] = useState(false);
+//     const [showAlert, setShowAlert] = useState(false);
+
+//     const handleInputChange = (event) => {
+//       const { name, value } = event.target;
+//       setUserFormData({ ...userFormData, [name]: value });
+//     };
+
+//     const handleFormSubmit = async (event) => {
+//       event.preventDefault();
+
+// export default LoginForm;
